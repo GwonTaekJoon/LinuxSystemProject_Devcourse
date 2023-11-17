df07f080ab830e2e0e4a17c8177e1253ed1d4b83
GwonTaekJoon <kyuzu123123@gmail.com>
2023-11-17 14:24:47 +0900

    implement signal

--- main.c
+++ main.c
@@ -5,6 +5,28 @@
 #include <gui.h>
 #include <input.h>
 #include <web_server.h>
+#include <string.h>
+
+/* 여기서 sigchld 핸들러 구현 */
+static void
+sigchldHandler(int sig)
+{   
+    int status;
+     int tmp_errno = errno;
+     pid_t childPid;
+
+    /*errno is a global variable provided by the standard library of language C,
+     indicating the cause of errors when system calls and library functions fail.*/
+
+    while ((childPid = waitpid(-1, &status, WNOHANG)) > 0) {
+        printf("childPid = %d terminated\n", childPid);
+    }
+
+    /*All child processes are terminated */
+    
+    errno = tmp_errno;
+    //restored errno
+}
 
 int main()
 {
@@ -14,7 +36,16 @@ int main()
     pid_t wpid;
 
     int status;
-    int savedErrno;
+
+    struct sigaction sa;
+
+    memset(&sa, 0, sizeof(sigaction));
+    sigemptyset(&sa.sa_mask);
+    sa.sa_handler = sigchldHandler;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        fprintf(stderr,"signal init error");
+        return 1;
+    }
 
     printf("main()...\n");
 
@@ -28,7 +59,7 @@ int main()
     
     printf("create_input()...\n");
     ipid = create_input();
-        
+
     printf("create_gui()...\n");
     gpid = create_gui();
 
--- ui/input.c
+++ ui/input.c
@@ -1,15 +1,86 @@
 #include <stdio.h>
 #include <sys/prctl.h>
+#include <execinfo.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ucontext.h>
+#include <unistd.h>
 
 #include <system_server.h>
 #include <gui.h>
 #include <input.h>
 #include <web_server.h>
+#include <execinfo.h>
+
+typedef struct _sig_ucontext {
+    unsigned long uc_flags;
+    struct ucontext *uc_link;
+    stack_t uc_stack;
+    struct sigcontext uc_mcontext;
+    sigset_t uc_sigmask;
+} sig_ucontext_t;
+
+void segfault_handler(int sig_num, siginfo_t * info, void * ucontext) {
+  void * array[50];
+  void * caller_address;
+  char ** messages;
+  int size, i;
+  sig_ucontext_t * uc;
+
+  uc = (sig_ucontext_t *) ucontext;
+
+  /* Get the address at the time the signal was raised */
+  caller_address = (void *) uc->uc_mcontext.rip;  // RIP: x86_64 specific     arm_pc: ARM
+
+  fprintf(stderr, "\n");
+
+  if (sig_num == SIGSEGV)
+    printf("signal %d (%s), address is %p from %p\n", sig_num, strsignal(sig_num), info->si_addr,
+           (void *) caller_address);
+  else
+    printf("signal %d (%s)\n", sig_num, strsignal(sig_num));
+
+  size = backtrace(array, 50);
+  /* overwrite sigaction with caller's address */
+  array[1] = caller_address;
+  messages = backtrace_symbols(array, size);
+
+  /* skip first stack frame (points here) */
+  for (i = 1; i < size && messages != NULL; ++i) {
+    printf("[bt]: (%d) %s\n", i, messages[i]);
+  }
+
+  free(messages);
+
+  exit(EXIT_FAILURE);
+}
+
 
 int input()
 {
     printf("input()...\n");
 
+
+    /*  signal */
+
+    struct sigaction sa;
+
+    memset(&sa, 0, sizeof(sigaction));
+    sigemptyset(&sa.sa_mask);
+    //safe init
+
+    sa.sa_flags = SA_RESTART | SA_SIGINFO;
+    sa.sa_sigaction = segfault_handler;
+
+    if(sigaction(SIGSEGV, &sa, NULL) == -1) {
+
+        fprintf(stderr,"signal init error");
+        exit(1);
+
+    }
+    
+    
     while (1) {
         sleep(1);
     }
